dc60e6722a26f6bfdd6aa884c0ceeb44a74bb8d3
Kgoering <kaleb.goering@gmail.com>
2014-09-16 17:37:43 -0500

    Updating to create a library release

--- setup.py
+++ setup.py
@@ -8,5 +8,13 @@ setup(
 	author_email = "eeg-signal-processor@googlegroups.com",
 	url = "",
 	decription = "Uses machine learning algorithms to predict what is thought",
-	long_description = "Takes either a transformed EEG signal or a raw EEG signal and can either learn it or predict its value, what is being thought in this signal. It can use a SVM, LDA, or QDA for the learning algorithm. The libraries needed: PyWavelets, Numpy, CSV, Scikit-Learn"
+	long_description = "Takes either a transformed EEG signal or a raw EEG signal and can either learn it or predict its value. It can use a SVM, LDA, or QDA for the learning algorithm. The libraries needed: PyWavelets, Numpy, CSV, Scikit-Learn"
+	classifiers = [
+		"Programming Language :: Python",
+	        "Programming Language :: Python :: 3",
+	        "Development Status :: 1.0.0",
+	        "Intended Audience :: Developers",
+	        "License :: OSI Approved :: Apache",
+	        "Operating System :: OS Independent",
+		]
 	)
